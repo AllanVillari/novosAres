08f5dd47e7dd6cde52eb304969ef01a69a06e5b8
Allan Villari <allan.villarinho@hotmail.com>
2024-05-24 19:41:43 -0300

    adicional buttons

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-// $(`#telefone`).mask(`(00) 00000-0000`, {
-//     placeholder: `(__) _____-____`
-// });
+$(`#telefone`).mask(`(00) 00000-0000`, {
+    placeholder: `(__) _____-____`
+});
 
-// $(`form`).validate({
-//     rules: {
-//         nome: { required: true },
-//         email: { required: true },
-//         mensagem: { required: true },
-//     },
-//     messages: {
-//         nome: `Por favor, insira o seu nome`,
-//         email: `Insira um e-mail válido`,
-//         mensagem: `Insira sua mensagem`
+$(`form`).validate({
+    rules: {
+        nome: { required: true },
+        email: { required: true },
+        mensagem: { required: true },
+    },
+    messages: {
+        nome: `Por favor, insira o seu nome`,
+        email: `Insira um e-mail válido`,
+        mensagem: `Insira sua mensagem`
 
-//     },
-//     submitHandler: function (form) {
-//         console.log(form)
-//     },
-//     invalidHandler: function (evento, validador) {
-//         let camposIncorretos = validador.numberOfInvalids();
-//         console.log(camposIncorretos)
-//     }
+    },
+    submitHandler: function (form) {
+        console.log(form)
+    },
+    invalidHandler: function (evento, validador) {
+        let camposIncorretos = validador.numberOfInvalids();
+        console.log(camposIncorretos)
+    }
 
-// })
+})
